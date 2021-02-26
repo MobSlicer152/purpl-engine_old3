826a59701698bc5cac14c3597e52913f1a86418d
MobSlicer152 <brambleclaw1414@gmail.com>
2021-02-25 20:28:17 -0500

    Created a nice script to make an object file from an archive

---
+++ make_embeddable.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Check args
+if [ $# -lt 1 ] || [ -z $1 ]; then
+	echo "This script objcopies an archive into an object file to be embedded in an executable"
+	echo "Usage: $0 <archive> [<object name>]"
+	exit
+fi;
+
+# Check if there's a different object file name given
+if [ $# -gt 1 ]; then
+	output=${2}
+else
+	output="embed.o"
+fi
+
+# Check platform to determine output format
+platform="$(uname -s)"
+if [ ${platform} == "CYGWIN*" ] || [ ${platform} == "MINGW*" ]; then
+	fmt="pe-x86-64" # I really hope this allows for MSVC compatibility
+else
+	fmt="elf64-x86-64"
+fi
+echo "Using output format ${fmt} because script is running under ${platform}"
+
+# Objcopy
+echo "Objcopying archive ${1} into ${output}"
+objcopy --input binary --output $fmt --binary-architecture i386:x86-64 --rename-section .data=.rodata,contents,alloc,load,readonly,data ${1} ${output}
